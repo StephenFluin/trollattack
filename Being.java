f27ac3463ba9b2a4a43e6f19950702cab3a10c9c
Stephen Fluin <stephen@mistuph.com>
2005-05-09 06:12:04 +0000

    Adding being superclass for players and mobs.

---
+++ Being.java
@@ -0,0 +1,20 @@
+/*
+ * Created on May 9, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+
+/**
+ * @author PeEll
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Being {
+	int hitPoints, maxHitPoints, hitSkill, hitDamage;
+	String shortDescription;
+	public String getShort() {
+		return shortDescription;
+	}
+}
